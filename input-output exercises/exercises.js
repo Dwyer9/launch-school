12603915982824f25c66de34c4728d94da6e5e31
Mathew Dwyer <mdwyer139@gmail.com>
2023-06-03 09:38:42 +1000

    complete input/output exercises

---
+++ input-output exercises/exercises.js
@@ -0,0 +1,15 @@
+// 1.
+// let name = prompt('What is your name?');
+// console.log(`Hello, ${name}!`);
+
+// 2.
+// let firstName = prompt('What is your first name?');
+// let surname = prompt('What is your surname?');
+// console.log(`Hello, ${firstName} ${surname}!`);
+
+let age = Number(prompt('How old are you?'));
+console.log(`You are ${age} years old`);
+console.log(`In 10 years, you will be ${age + 10} years old`);
+console.log(`In 20 years, you will be ${age + 20} years old`);
+console.log(`In 30 years, you will be ${age + 30} years old`);
+console.log(`In 40 years, you will be ${age + 40} years old`);
