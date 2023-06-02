42adb9290073414374dda81de1cfad4dcbc531c9
Mathew Dwyer <mdwyer139@gmail.com>
2023-06-03 09:23:37 +1000

    complete variables exercises

---
+++ variables-exercises/script.js
@@ -0,0 +1,21 @@
+// 1.
+let name = 'Victor';
+console.log(`Good Morning, ${name}`);
+console.log(`Good Afternoon, ${name}`);
+console.log(`Good Evening, ${name}`);
+
+// 2.
+let age = 20;
+console.log(`You are ${age} years old`);
+console.log(`In 10 years, you will be ${age + 10} years old`);
+console.log(`In 20 years, you will be ${age + 20} years old`);
+console.log(`In 30 years, you will be ${age + 30} years old`);
+console.log(`In 40 years, you will be ${age + 40} years old`);
+
+// 3. Error - outside scope
+
+// 4. Error - assignment to constant variable
+
+// 5. bar - foo is intialised in global scope and then initialised again in block scope - different variable
+
+// 6. No error - same as above, initialised same variable in different scopes.
